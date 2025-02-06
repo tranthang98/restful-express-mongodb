ad8d020e9ecc5bb1f317710155a909c4b9693d78
tranthang98 <tranthang98hust@gmail.com>
2025-02-06 15:47:22 +0700

    create mongodb connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,44 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
+const mongoose = require('mongoose');
 
 // create the connection to database
-// const connection = mysql.createConnection({
+// const connection = mysql.createPool({
 //     host: process.env.DB_HOST,
 //     port: process.env.DB_PORT,  //default: '3306' 3306
 //     user: process.env.DB_USER, //default: empty
 //     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
 // });
 
-// create the connection to database
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,  //default: '3306' 3306
-    user: process.env.DB_USER, //default: empty
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const dbState = [{
+    value: 0,
+    label: "disconnected"
+},
+{
+    value: 1,
+    label: "connected"
+},
+{
+    value: 2,
+    label: "connecting"
+},
+{
+    value: 3,
+    label: "disconnecting"
+}];
 
+
+const connection = async () => {
+    try {
+        await mongoose.connect('mongodb://root:123456@localhost:27018');
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+    } catch (error) {
+        console.log(">>> Error connection DB: ", error)
+    }
+}
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine')
 const webRouters = require('./routes/web');
+const connection = require('./config/database');
 
 const app = express();// app express
 const port = process.env.PORT || 8888
@@ -14,6 +15,8 @@ configViewEngine(app);
 
 //khai báo route
 app.use('/', webRouters);
+
+connection();
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
